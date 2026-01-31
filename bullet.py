f7165824ef0659085142d3771f03934dbc8e56b5
YaRo <roman.yatsenko@hneu.net>
2026-01-31 16:32:43 +0200

    Add Bullet class

---
+++ bullet.py
@@ -0,0 +1,22 @@
+import pygame as pg
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """Клас для управління снарядами, якими стріляє корабель"""
+
+    def __init(self, ai_game):
+        """Створює об'єкт снаряду в поточні позиції корабля"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Створюємо снаряд в позиції (0, 0) і призначаємо правильну позицію
+        self.rect = pg.Rect(
+            0, 0, self.settings.bullet_width, self.settings.bullet_height
+        )
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Позиція снаряду зберігається як float
+        self.y = float(self.rect.y)
