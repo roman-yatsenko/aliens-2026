735151b0832cbbebc08e0ff0828c98fa75cbbd3e
YaRo <roman.yatsenko@hneu.net>
2026-02-14 16:49:07 +0200

    Add game over

--- start.py
+++ start.py
@@ -38,9 +38,10 @@ class AlienInvasion:
         """Запуск основного циклу гри"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -127,16 +128,16 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Обробляє зіткнення корабля з прибульцем"""
-        self.stats.ships_left -= 1
-
-        self.aliens.empty()
-        self.bullets.empty()
-
-        self._create_fleet()
-        self.ship.center_ship()
-
-        # Пауза
-        sleep(0.5)
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
+            self.aliens.empty()
+            self.bullets.empty()
+            self._create_fleet()
+            self.ship.center_ship()
+            # Пауза
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_aliens(self):
         """Оновлює позиції всіх прибульців флоту"""
