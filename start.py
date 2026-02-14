0a859509eee09e12feccb16069ffcc5c9065d9f5
YaRo <roman.yatsenko@hneu.net>
2026-02-14 16:07:40 +0200

    Add ship-aliens collisions

--- start.py
+++ start.py
@@ -118,6 +118,10 @@ class AlienInvasion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Перевірка колізій "прибулець-корабель"
+        if pg.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
+
     def _update_bullets(self):
         """Оновлення снарядів"""
         self.bullets.update()
