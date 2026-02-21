758ea2e97fb03bc371796807b8f47495182b4117
YaRo <roman.yatsenko@hneu.net>
2026-02-21 16:03:16 +0200

    Add speed reset

--- start.py
+++ start.py
@@ -110,7 +110,8 @@ class AlienInvasion:
         """Запускає нову гру коли натиснуто Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            # Скидання ігрової статистики
+            # Скидання ігрової статистики та налаштувань
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
