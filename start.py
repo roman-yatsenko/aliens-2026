f77769b7d4b14321396bdc7671673d9b9ec09b4a
YaRo <roman.yatsenko@hneu.net>
2026-02-21 16:43:29 +0200

    Improve score adding

--- start.py
+++ start.py
@@ -115,6 +115,7 @@ class AlienInvasion:
             # Скидання ігрової статистики та налаштувань
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
+            self.sb.prepare_score()
             self.stats.game_active = True
 
             self.aliens.empty()
@@ -186,7 +187,8 @@ class AlienInvasion:
         # Перевірка потраплянь у прибульців
         collisions = pg.sprite.groupcollide(self.bullets, self.aliens, True, True)
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prepare_score()
 
         if not self.aliens:
